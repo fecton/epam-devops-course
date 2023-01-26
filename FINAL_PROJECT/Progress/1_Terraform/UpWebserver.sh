0103461d1d820bead010f91cf18407c46016ece4
Andrew Fecton <bopongamer@gmail.com>
2023-01-26 21:17:18 +0100

    removed directory

--- FINAL_PROJECT/Progress/1_Terraform/UpWebserver.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-yum -y update
-yum -y install httpd
-myip=`curl http://169.254.69.254/latest/meta-data/local-ipv4`
-echo "Web server with ip: $ip" > /var/www/html/index.html
-sudo service https start
-chkconfig httpd on
